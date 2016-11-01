cbb8c5df8c4ee13dbc4b48392a337e78a42ca9d7
Ken Duigraha Putra <email": null>
2016-11-01 11:22:21 +0700

    updated transaction controller : create multiple booklist

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -51,34 +51,20 @@ let formNewData = (req, res, next) => {
 }
 
 let createNewData = (req, res, next) => {
-  console.log(req.body.booklist);
+
   ModelTransaction.create({
     days: req.body.days,
     memberid: req.body.memberid,
     out_date: req.body.out_date,
     due_date: req.body.due_date,
     in_date: req.body.in_date,
-    fine: req.body.fine
-    // booklist: req.body.booklist
+    fine: req.body.fine,
+    booklist: req.body.booklist
   }, (err, transaction) => {
     if (err) {
       console.log(err);
     } else {
-      // console.log(transaction);
-      // console.log(req.body.memberid);
-      ModelCustomer.
-        findOne({_id: req.body.memberid}).
-        populate({
-          path: 'transactions',
-
-          populate: {path : 'transactions'}
-        }).
-        exec((err, data) => {
-          if(err) console.log(err);
-          console.log(data);
-          // res.json(transaction)
-        })
-      
+      console.log(transaction);
       res.redirect('/transaction')
       // res.json(books);
     }
